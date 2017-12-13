33a56629b93569bb9113214e2dfbe5335beee79f
Silverath <pabvazzam@gmail.com>
2017-12-13 14:08:23 +0100

    Error

--- ApiEGC/api/resources.py
+++
@@ -1,9 +0,0 @@
-from tastypie.resources import ModelResource
-from api.models import Voto
-from tastypie.authorization import Authorization
-
-class VotoResource(ModelResource):
-    class Meta:
-        queryset = Voto.objects.all()
-        resource_name = 'voto'
-        authorization = Authorization()
