da401de66e1611c9be16079340701e154694c003
juasanpar <juasanpar@hotmail.com>
2017-12-17 19:37:04 +0100

    Update views.py

--- project/AlmacenamientoVotos/api/views.py
+++ project/AlmacenamientoVotos/api/views.py
@@ -10,13 +10,10 @@ def insertVoteWeb(request, id_poll, id_user, id_questionOption):
     for option in questionOptions:
         res = option.split("-")
         decision = res[0]
-        print decision
         priority = res[1]
         voto = Vote.objects.create(token = id_user, vote_type = VoteType.objects.filter(id = 1).get())
         questionOp = QuestionOption.objects.filter(id = decision).get()
         op = OptionPerVote.objects.create(vote = voto, question_option = questionOp)
-        
-        print op.id
-        
+                
     html = "<html><body>Voto de web insertado con exito</body></html>"
     return HttpResponse(html)
