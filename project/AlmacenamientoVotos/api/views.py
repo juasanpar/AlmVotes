a3cbdcf9a49af33c687b3163d38ac0feb675da0b
juasanpar <juasanpar@hotmail.com>
2017-12-17 19:15:25 +0100

    Update views.py
    Se ha añadido la función para insertar los votos en la BD

--- project/AlmacenamientoVotos/api/views.py
+++ project/AlmacenamientoVotos/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from models import *
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+def insertVoteWeb(request, id_poll, id_user, id_questionOption):
+    
+    questionOptions= id_questionOption.split("&");
+    
+    for option in questionOptions:
+        res = option.split("-")
+        decision = res[0]
+        print decision
+        priority = res[1]
+        voto = Vote.objects.create(token = id_user, vote_type = VoteType.objects.filter(id = 1).get())
+        questionOp = QuestionOption.objects.filter(id = decision).get()
+        op = OptionPerVote.objects.create(vote = voto, question_option = questionOp)
+        
+        print op.id
+        
+    html = "<html><body>Voto de web insertado con exito</body></html>"
+    return HttpResponse(html)
