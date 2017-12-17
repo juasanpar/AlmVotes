e168c2a7818d57b7fb57361daeb045f8fb574604
juasanpar <juasanpar@hotmail.com>
2017-12-17 19:10:15 +0100

    Eliminar todo
    Se están reorganizando los ficheros del proyecto

--- project/Workspace/api/resources.py
+++
@@ -1,9 +0,0 @@
-from tastypie.resources import ModelResource
-from api.models import vote
-from tastypie.authorization import Authorization
-
-class VoteResource(ModelResource):
-    class Meta:
-        queryset = vote.objects.all()
-        resource_name = 'vote'
-        authorization = Authorization()
