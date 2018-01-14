60591a53076bf6f3e302b5b79c4360c933a1cbab
juasanpar <juasanpar@hotmail.com>
2018-01-14 19:51:20 +0100

    Update 0001_initial.py

--- project/AlmacenamientoVotos/almvotes/migrations/0001_initial.py
+++ project/AlmacenamientoVotos/almvotes/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
             name='Ca',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', fernet_fields.fields.EncryptedCharField(max_length=100)),
+                ('name',models.CharField(max_length=100)),
             ],
             options={
                 'db_table': 'ca',
